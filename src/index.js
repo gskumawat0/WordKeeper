d22e2db21262fb7b7f087a388aa2a0872b4a7bc8
Gouri Shankar Kumawat <gskumawat555@gmail.com>
2021-06-10 01:01:05 +0530

    added App.js with root node

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App1 from "./App1";
+import App from "./App";
 
-ReactDOM.render(<App1 />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
